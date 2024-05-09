ce1a3f651a9eb45f1db97e433a63d058a987951a
Kan <kanalovefootball@gmail.com>
2024-05-09 13:40:39 +0700

    js:ex1-2-3-4

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,12 @@
 //Exercise #1: Variable Creation
-//Start coding here
+let name = "Karter";
+let lastname = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+const restaurantName = "Chef's Table by Chef James";
+const totalBill = 15000;
+const activeRestaurant = 1500;
 
 //Exercise #2: Variable Reassignment
-//Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+totalBill = 5000;
+activeRestaurant = 2500;
--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,5 @@
 // Exercise #3: Celsius to Fahrenheit
 let celsius = 30;
-
-// Start coding here;
-
 let fahrenheit;
+fahrenheit = celsius * 1.8 + 32;
+console.log(fahrenheit);
