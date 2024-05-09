997621d404de8982c2c232a3f593b0a028323312
TheeradetSukprem <161497900+TheeradetSukprem@users.noreply.github.com>
2024-05-09 12:59:25 +0700

    Gang Ex1-4 com

--- ex-4.js
+++ ex-4.js
@@ -1,13 +1,18 @@
 //ให้ Comment ต่อท้ายถ้าหากว่าเป็น Expression แต่ถ้าหากไม่เป็นให้ปล่อยว่างเอาไว้ตามตัวอย่างนี้
 50 + 12; // Expression
 
-//Exercise #4: What is Expression Start coding here
-130 + 300;
+// Exercise #4: What are Expressions
+130 + 300; // Expression
+
 let zero = 1000 - 1000;
-20 * 120;
-1000 / 2;
-typeof true;
+
+20 * 120; // Expression
+1000 / 2; // Expression
+
+typeof true; 
 let name = "John";
-3 > 5;
-10 == 100;
-200;
+
+3 > 5; // Expression
+10 == 100; // Expression
+
+200; // Expression
