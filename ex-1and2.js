047db2328a8c7069d215852c3ba9d4fc3fdb9a4d
Por <Poramaporn2540@gmail.com>
2024-05-29 14:02:48 +0700

    finished exercise

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,16 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let firstName = `Karter`;
+let lastName = `Anderson`;
+let userAddress = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+const resName = `Chef's Table by Chef James`;
+const totalPrice = 15000;
+const numberRes = 1500;
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+userAddress = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+totalPrice = 5000;
+numberRes = 2500;
+
