4d0c2c0ce8410357c63b8dfd51176f32a3a885f6
Apisit <fu.apisit@gmail.com>
2025-02-24 14:03:41 +0700

    Feat add answer ex1-4

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,14 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let name = "Karter"
+let surname = "Anderson"
+let location = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai"
+const restaurantsName = "Chef's Table by Chef James"
+const totalAmt = 15000
+const remainingCount = 15000
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+location = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641"
+totalAmt = 5000
+remainingCount = 2500
